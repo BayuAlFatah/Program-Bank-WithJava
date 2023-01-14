b494b42d2b19ecb0e72766d4c61bb57eb14d39ac
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-14 15:44:51 +0700

    menambahkan logic login pada main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,27 @@
 package bank1.Bank;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         var isbenar = true;
         Scanner inputUser = new Scanner(System.in);
         System.out.println("Masukkan UserName anda : ");
+        var nama = inputUser.next();
+        System.out.println("Masukkan No Pin Anda : ");
+        var pin = inputUser.nextInt();
+        try{
+            if (pin != 214441900){
+                System.out.println("No Pin Salah Silahkan Masukkan No Pin Yang Benar");
+                main(args);
+            }
+        }catch (Exception e){
+            System.out.println("Silahkan Masukkan Pin dengan Benar ");
+            main(args);
+        }
+
 
     }
 }
