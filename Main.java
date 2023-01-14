e83609f62d3aece208c3fd9963e78ed1693bcc43
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-14 15:24:49 +0700

    menambahkan field dan method pada main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 package bank1.Bank;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Bank b = new Bank();
 
-      System.out.println("nama = "+ b.Nama);
-     System.out.println("CEK SALDO = " + b.Saldo);
- System.out.println("Nomor Rekening = " + b.NoRek);
-
- System.out.println("Saldo awal = " + b.Saldo);
-       b.Setor(5000);
-        b.Tarik(5000);
-
+        var isbenar = true;
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Masukkan UserName anda : ");
 
     }
 }
